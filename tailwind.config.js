e515c524956149f24324ed006773042332638923
Artyom <a.gilemyanov@gmail.com>
2025-05-15 00:13:52 +0300

    tailwind.config.ts translate comments to English.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,13 @@
 module.exports = {
     content: [
       './index.html',
-      './src/**/*.{vue,js,ts,jsx,tsx}', // Указываем пути для сканирования классов
+      './src/**/*.{vue,js,ts,jsx,tsx}', // Path to your frontend components
     ],
     theme: {
       extend: {
         colors: {
           gray: {
-            100: '#f3f4f6', // Явно добавляем gray-100, если он отсутствует
+            100: '#f3f4f6', // Light gray setup
           },
         },
       },
